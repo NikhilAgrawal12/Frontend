12813ed0c460a446c047c39af432bc996a095bdd
Nikhil Agrawal <nikhilagrawal1289@gmail.com>
2026-03-05 12:27:07 -0500

    Update links to match the router

--- React/ecommerce-project/src/App.jsx
+++ React/ecommerce-project/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
       <Routes>
-          <Route index element={<HomePage />} />
+          <Route path="/" element={<HomePage />} />
           <Route path="checkout" element={<CheckoutPage />} />
       </Routes>
   );
